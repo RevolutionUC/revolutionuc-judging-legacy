f9603c0a101660c86de871d2ea9b64970e436fa6
Dev Agrawal <devagrawal09@gmail.com>
2020-01-18 18:06:11 -0500

    parse csv file and output to json

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 const csvToJson = require('csvjson-csv2json')
 const fs = require('fs')
 
+const titleColumn = 'Submission Title'
+const categoryColumn = 'Opt-in prize'
+
 const config = {
     generalGroupCount: 0,
     generalJudgesPerGroup: 0,
-    generalGroupsPerProject: 0,
-    categories: []
+    generalGroupsPerProject: 0
 }
+
+const Submissions = {}
+const Categories = []
 
 try {
     const data = fs.readFileSync('submission-data.csv')
     const csv = data.toString()
     const json = csvToJson(csv)
+    json.forEach(submission=> {
+
+        const submissionTitle = submission[ titleColumn ]
+        const category = submission[ categoryColumn ]
+        submission[ categoryColumn ] = undefined
+        
+        const existingSubmission = Submissions[submissionTitle]
+
+        if( existingSubmission ) {
+            existingSubmission.categories.push( category )
+        } else {
+            submission.categories = [ category ]
+            Submissions[submissionTitle] = submission
+        }
+
+        if( !Categories.includes( category ) )
+            Categories.push( category )
+
+    })
+
+    fs.writeFileSync('submission-data.json', JSON.stringify({ Submissions, Categories }, null, 2))
 } catch( err ) {
     console.error(err)
 }
